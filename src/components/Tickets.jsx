1dd2799bc3c7545e38fb9c9570f733d8741f387b
develop <dev-juan-eloper@gmail.com>
2023-04-07 14:10:53 -0600

    Fix

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -9,10 +9,10 @@ export const Tickets = ({ send, state }) => {
   return (
     <div className='Tickets'>
       <p className='Tickets-description description'>Gracias por volar con book a fly 💚</p>
-      {state.context.passengers.map((item, index) => <p key={index+1}>{item}</p>)}
       <div className='Tickets-ticket'>
         <div className='Tickets-country'>{state.context.selectedCountry}</div>
         <div className='Tickets-passengers'>
+      {state.context.passengers.map((item, index) => <p key={index+1}>{item}</p>)}
           <span>✈</span>
           </div>
           </div>
---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+const fetchCountries = () => {
+  fetch("https://restcountries.com/v3.1/region/ame")
+  .then((response) => response.json());
+}
+
+export { fetchCountries };
